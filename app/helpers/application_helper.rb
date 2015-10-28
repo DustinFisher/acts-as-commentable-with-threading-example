a0ddb7405d881e148f7f87da11e13c7c3b0fca85
Dustin Fisher <dustin@dustinfisher.com>
2015-10-28 18:47:14 -0400

    adding bootstrap style to flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def bootstrap_flash_class(flash_type)
+    { success: 'alert-success',
+      error:   'alert-danger',
+      alert:   'alert-warning',
+      notice:  'alert-info'
+    }[flash_type.to_sym] || flash_type.to_s
+  end
 end
